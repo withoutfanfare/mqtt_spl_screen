b08111173df583fe51608804e95e74d8af1b7858
DannyHarding <danny@stuntrocket.co>
2020-09-05 23:08:48 +0100

    Initial

---
+++ pizero_screen_subscriber.py
@@ -0,0 +1,49 @@
+import paho.mqtt.client as mqtt
+import logging
+
+from spl_screen import SPScreen
+from pizero_screen_config import pizero_screen_config
+
+logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+
+MQTT_BROKER = pizero_screen_config["mqtt_broker"]
+MQTT_TOPIC = pizero_screen_config["mqtt_topic"]
+
+defaultBg = (16, 23, 31)
+initBg = (78, 3, 97)
+errorBg = (115, 11, 0)
+successBg = (0, 102, 42)
+pendingBg = (94, 0, 115)
+
+defaultColor = '#FFFFFF'
+
+myScreen = SPScreen()
+client = mqtt.Client()
+
+def on_connect(client, userdata, flags, rc):
+    # print("Connected with result code "+str(rc))
+    myScreen.message('Connected', defaultColor, initBg)
+    client.subscribe(MQTT_TOPIC)
+
+
+def on_message(client, userdata, msg):
+    m = msg.payload.decode('utf-8')
+    bg = defaultBg
+    if "UP @" in m:
+        bg = successBg
+    if "DOWN @" in m:
+        bg = errorBg
+    if "OK" in m:
+        bg = pendingBg
+    if "REBOOT" in m:
+        check_call(['sudo', 'reboot'])
+    if "SHUTDOWN" in m:
+        check_call(['sudo', 'poweroff'])
+
+    myScreen.message(m, defaultColor, bg)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(MQTT_BROKER, 1883)
+client.loop_forever()
---
+++ spl_screen.py
@@ -0,0 +1,75 @@
+import digitalio
+import board
+import time
+
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
+class SPScreen(object):
+
+    def __init__(self):
+
+        self.cs_pin = digitalio.DigitalInOut(board.CE0)
+        self.dc_pin = digitalio.DigitalInOut(board.D25)
+
+        self.reset_pin = None
+
+        # Config for display baudrate (default max is 24mhz):
+        self.BAUDRATE = 64000000
+        self.spi = board.SPI()
+        self.defaultBg = (16, 23, 31)
+
+        # Create the ST7789 display:
+        self.disp = st7789.ST7789(
+            self.spi,
+            cs=self.cs_pin,
+            dc=self.dc_pin,
+            rst=self.reset_pin,
+            baudrate=self.BAUDRATE,
+            width=135,
+            height=240,
+            x_offset=53,
+            y_offset=40,
+        )
+
+        # Make sure to create image with mode 'RGB' for full color.
+        self.height = self.disp.width  # swap height/width to rotate it to landscape
+        self.width = self.disp.height
+        self.image = Image.new("RGB", (self.width, self.height))
+        self.rotation = 270
+
+        # Get drawing object to draw on image.
+        self.draw = ImageDraw.Draw(self.image)
+
+        self.clear(self.defaultBg)
+
+        self.padding = 5
+        self.top = self.padding
+        self.bottom = self.height - self.padding
+        self.x = 5
+
+        self.font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
+
+        self.backlight = digitalio.DigitalInOut(board.D22)
+        self.backlight.switch_to_output()
+        self.backlight.value = True
+        self.auto_brightness = False
+        self.brightness = 0.1
+
+
+    def backlightOff(self):
+        self.backlight.value = False
+
+
+    def clear(self, bg):
+        self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=bg)
+        self.disp.image(self.image, self.rotation)
+
+
+    def message(self, message, color, bg):
+        self.clear(bg)
+
+        self.y = self.top
+        self.draw.text((self.x, self.y), message, font=self.font, fill=color)
+        self.disp.image(self.image, self.rotation)
+        time.sleep(0.1)
